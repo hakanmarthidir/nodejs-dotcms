bc6ae570d6bfbd1959133e60976fc96858d21274
hakan hidir <hakan_hidir@hotmail.com>
2021-02-02 13:13:26 +0100

    refactored query creator and discovered some new queries

--- myDotCmsBackend/routes/blog.js
+++ myDotCmsBackend/routes/blog.js
@@ -8,9 +8,9 @@ var BlogCard = require('../models/blogcard');
 
 
 const root = 'http://localhost:8080'
-const blogurl = root + '/api/content/render/false/type/json/query/+contentType:Blog%20+(conhost:48190c8c-42c4-46af-8d1a-0cd5db894797%20conhost:SYSTEM_HOST)%20+languageId:1%20+deleted:false%20+working:true/orderby/modDate%20desc/limit/4/offset/0/type/json'
+const blogroot = root + '/api/content/render/false/type/json/query/';
 const blogdetailUrl = root + '/api/content/id/'
-const myCache = new NodeCache({ stdTTL: 600, checkperiod: 300 });
+const myCache = new NodeCache({ stdTTL: 300, checkperiod: 150 });
 
 function escapeRegExp(string) {
     return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
@@ -58,6 +58,21 @@ function functionMapToBlogDetail(blogResult) {
 
 }
 
+function createLimitUrl(limit = 6, offset = 0) {
+    return `/limit/${limit}/offset/${offset}/type/json`;
+}
+
+function createDotCmsQuery(contentType = "Blog", languageId = "1", orderby = "modDate%20desc", offset = 0, limit = 6, tagSearch=null) {
+    let query = ""
+    if(tagSearch === null){
+        query = blogroot + `+contentType:${contentType}%20+languageId:${languageId}%20+deleted:false%20+working:true%20+live:true/orderby/${orderby}` + createLimitUrl(limit, offset);
+    } else {
+        query = blogroot + `+contentType:${contentType}%20+Blog.tags:%22${tagSearch}%22%20+languageId:${languageId}%20+deleted:false%20+working:true%20+live:true/orderby/${orderby}` + createLimitUrl(limit, offset);
+    }
+    return query;
+}
+
+
 /* GET users listing. */
 router.get('/blog', function (req, res, next) {
 
@@ -65,22 +80,81 @@ router.get('/blog', function (req, res, next) {
 
     if (cachedValue == undefined) {
         //there is no cached value
-        axios.get(blogurl)
+        let url = createDotCmsQuery("Blog", "1", null, 0, 6);
+        axios.get(url)
             .then(response => {
                 let overview = functionMapToBlogOverview(response.data.contentlets);
                 myCache.set("blogs", overview);
-                res.send(overview);
+                res.json(overview);
             })
             .catch(error => {
                 console.log(error);
             });
     }
     else {
-        res.send(cachedValue);
+        res.json(cachedValue);
     }
 });
 
-router.get('/blog/:id', function (req, res, next) {
+//To get last 6 Blogs -> http://localhost:3000/api/blog/lastblogsbycount/6
+router.get('/blog/lastblogsbycount/:count', function (req, res) {
+
+    let count = req.params.count;
+    if (count) {
+
+        let cacheKey = "lastblogsbycount-" + count
+        let cachedValue = myCache.get(cacheKey);
+        if (cachedValue == undefined) {
+            let url = createDotCmsQuery("Blog", "1", null, 0, count);
+            axios.get(url)
+                .then(response => {
+                    let overview = functionMapToBlogOverview(response.data.contentlets);
+                    myCache.set(cacheKey, overview);
+                    res.json(overview);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+        else {
+            res.json(cachedValue);
+        }
+    }
+});
+
+// start : starting index, 0 means first item 
+// count : how many items you want
+//Sample1 : http://localhost:3000/api/blog/start/0/count/3
+//Sample2 : http://localhost:3000/api/blog/start/2/count/5 
+router.get('/blog/start/:offset/count/:limit', function (req, res) {
+
+    let limit = req.params.limit;
+    let offset = req.params.offset;
+
+    if (limit && offset) {
+
+        let cacheKey = "offset-" + offset + "limit-" + limit
+        let cachedValue = myCache.get(cacheKey);
+        if (cachedValue == undefined) {
+            let url = createDotCmsQuery("Blog", "1", null, offset, limit);
+            axios.get(url)
+                .then(response => {
+                    let overview = functionMapToBlogOverview(response.data.contentlets);
+                    myCache.set(cacheKey, overview);
+                    res.json(overview);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+        else {
+            res.json(cachedValue);
+        }
+    }
+});
+
+//Sample 1: http://localhost:3000/api/blog/detail/4badeaad-809e-468d-9cfe-8cbb20b5d11a
+router.get('/blog/detail/:id', function (req, res) {
 
     let id = req.params.id;
 
@@ -92,19 +166,48 @@ router.get('/blog/:id', function (req, res, next) {
         if (cachedValue == undefined) {
 
             let detailUrl = blogdetailUrl + id
-
+            
             axios.get(detailUrl)
                 .then(response => {
                     let detail = functionMapToBlogDetail(response.data.contentlets);
                     myCache.set(cacheKey, detail);
-                    res.send(detail);
+                    res.json(detail);
                 })
                 .catch(error => {
                     console.log(error);
                 });
 
         } else {
-            res.send(cachedValue);
+            res.json(cachedValue);
+        }
+    }
+});
+
+//Sample 1: http://localhost:3000/api/blog/lastblogsbytag/ta/1
+router.get('/blog/lastblogsbytag/:tag/:count', function (req, res) {
+
+    let tag = req.params.tag;
+    console.log(tag)
+    if (tag) {
+        let count = req.params.count;
+        console.log(count)
+        let tagStr= tag.toString();
+        let cacheKey = "lastblogsbytag-" + tagStr
+        let cachedValue = myCache.get(cacheKey);
+        if (cachedValue == undefined) {
+            let url = createDotCmsQuery("Blog", "1", "score,modDate%20desc", null, count, tagStr);
+            axios.get(url)
+                .then(response => {
+                    let overview = functionMapToBlogOverview(response.data.contentlets);
+                    myCache.set(cacheKey, overview);
+                    res.json(overview);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+        else {
+            res.json(cachedValue);
         }
     }
 });
